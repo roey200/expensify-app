57bf9ac6f3a33f064be90597e095102a19615976
Roi Mass <rmass@atlasprofessionals.com>
2020-08-27 22:53:45 +0200

    Setup prod webpack

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git commands
+
+git init - create a new git repo
+git status - View the changes to your porject code
+git add - Add files to staging are
+git commit - Created a new commit with files from staging area
+git log - View recent commits
--- src/app.js
+++ src/app.js
@@ -12,17 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}))
-store.dispatch(addExpense({ description: 'Rent Bill', amount: 109500}))
-
-
-
-const state = store.getState()
-const visibileExpense = getVisibileExpense(state.expenses, state.filters)
-console.log(visibileExpense)
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
