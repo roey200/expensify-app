0994f680c079f1309a6626cac95fe33d3a828be8
Roi Mass <rmass@atlasprofessionals.com>
2020-09-02 21:31:40 +0200

    Adding test and fetch func

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startAddExpense, editExpense, removeExpense } from '../../actions/expenses'
+import { startAddExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt }
+    })
+    database.ref('expenses').set(expensesData).then(() => done())
+})
 
 test('Should setup remove expense object', () => {
     const action = removeExpense({ id: '123abc' })
@@ -79,6 +87,27 @@ test('Should add expense with defaults to database and store', (done) => {
         expect(snapshot.val()).toEqual(expenseDeafults)
         done()
     })
+})
+
+test('should set up set expense action with data', () => {
+    const action = setExpenses(expenses)
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
+})
+
+test('should fetch the expenses from firebase', (done) => {
+    const store = createMockStore({})
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        })
+        done()
+    })
+
 })
 
 // test('Shoud setup add expense action object with default values', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-process.env.NODE.ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if ( process.env.NODE.ENV === 'test' ){
-    require('dotenv').config({ path: '.env.test' })
-} else if ( process.env.NODE.ENV === 'development' ){
-    require('dotenv').config({ path: '.env.development' })
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
 
 
