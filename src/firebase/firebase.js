ebf1ac5f8f9cf9fd0ace31af1dcdf4db472ae8ea
Roi Mass <rmass@atlasprofessionals.com>
2020-09-03 22:56:11 +0200

    Add private route and login logout

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KE,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -10,13 +10,14 @@ const firebaseConfig = {
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
-
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database()
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
-export { firebase, database as default }
+export { firebase, googleAuthProvider, database as default }
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
